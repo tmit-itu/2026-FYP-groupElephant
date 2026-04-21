fa5c0f24d715329c2af7abd878836deed394af3e
Klara <mklaruh@gmail.com>
2026-04-21 20:54:46 +0200

    Edited extended version with diameter feature

--- src/exctract_features_extended.py
+++ src/exctract_features_extended.py
@@ -5,31 +5,34 @@ from hair_removal import remove_hair
 from feature_asymmetry import get_assymetry
 from feature_border import get_border
 from feature_texture import mean_gradient
+from feature_diameter import get_diameter
 
 def extract_extended():
     metadata = pd.read_csv("../metadata.csv")
     diagnosis_mapping = {'MEL': 1, 'BCC': 1, 'SCC': 1, 'NEV': 0, 'ACK': 0, 'SEK': 0}
-    metadata['cancer'] = metadata['cancer'].map(diagnosis_mapping)
+    metadata["cancer"] = metadata["diagnostic"].map(diagnosis_mapping)
 
     results = []
 
     for index, row in metadata.iterrows():
-        img_id = row["img_id"].str[:-4]
+        img_id = os.path.splitext(row["img_id"])[0]
         img = cv2.imread(f"../data/imgs/{img_id}.png")
         mask = cv2.imread(f"../data/masks/{img_id}_mask.png", 0)
 
         if img is not None and mask is not None:
             img_clean, _ = remove_hair(img)
 
-            feature_a = get_asymmetry(mask)
+            feature_a = get_assymetry(mask)
             feature_b = get_border(mask)
             feature_t = mean_gradient(img_clean, mask)
+            feature_d = get_diameter(mask)
             
             results.append({
                 "img_id": img_id,
                 "asymmetry": feature_a,
                 "border": feature_b,
                 "texture": feature_t,
+                "diameter": feature_d,
                 "cancer": row["cancer"]
             })
     

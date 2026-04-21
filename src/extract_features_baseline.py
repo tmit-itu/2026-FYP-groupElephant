b425e57533ab259367aedf25d17bdaf7d349feac
Klara <mklaruh@gmail.com>
2026-04-21 20:51:48 +0200

    Edited extended version with diameter feature

--- src/extract_features_baseline.py
+++ src/extract_features_baseline.py
@@ -1,41 +1,43 @@
 import pandas as pd
-import os
 import cv2
+import os
+from hair_removal import remove_hair
 from feature_asymmetry import get_assymetry
 from feature_border import get_border
 from feature_texture import mean_gradient
 from feature_diameter import get_diameter
 
-
-def extract_all():
+def extract_extended():
     metadata = pd.read_csv("../metadata.csv")
-    diagnosis_mapping = {"MEL":1, "BCC":1, "SCC":1, "NEV":0, "ACK":0, "SEK":0}
+    diagnosis_mapping = {'MEL': 1, 'BCC': 1, 'SCC': 1, 'NEV': 0, 'ACK': 0, 'SEK': 0}
     metadata["cancer"] = metadata["diagnostic"].map(diagnosis_mapping)
 
     results = []
 
     for index, row in metadata.iterrows():
-        img_id = os.path.splitext(row["img_id"])[0] 
-
+        img_id = os.path.splitext(row["img_id"])[0]
         img = cv2.imread(f"../data/imgs/{img_id}.png")
-        mask = cv2.imread(f"../data/masks/{img_id}_mask.png", cv2.IMREAD_GRAYSCALE)
+        mask = cv2.imread(f"../data/masks/{img_id}_mask.png", 0)
 
-        feature_a = get_assymetry(mask)
-        feature_b = get_border(mask)
-        feature_t = mean_gradient(img, mask)
-        feature_d = get_diameter(mask)
+        if img is not None and mask is not None:
+            img_clean, _ = remove_hair(img)
 
-
-        results.append({"img_id": img_id,
-            "asymmetry": feature_a,
-            "border": feature_b,
-             "texture": feature_t,
-            "diameter": feature_d,
-            "cancer": row["cancer"]})
+            feature_a = get_assymetry(mask)
+            feature_b = get_border(mask)
+            feature_t = mean_gradient(img_clean, mask)
+            feature_d = get_diameter(mask)
+            
+            results.append({
+                "img_id": img_id,
+                "asymmetry": feature_a,
+                "border": feature_b,
+                "texture": feature_t,
+                "diameter": feature_d,
+                "cancer": row["cancer"]
+            })
     
     df_features = pd.DataFrame(results)
-    df_features.to_csv("../results/features.csv", index=False)
+    df_features.to_csv("../results/features_extended.csv", index=False)
 
 if __name__ == "__main__":
-    extract_all()
-    
+    extract_extended()

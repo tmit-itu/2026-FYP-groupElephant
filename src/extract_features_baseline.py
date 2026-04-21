e46c8ef7dff1cadfe0ce6015bda3ce3274f6f5f7
Klara <mklaruh@gmail.com>
2026-04-21 20:47:26 +0200

    Edited the extract_features_baseline with the diameter feature

--- src/extract_features_baseline.py
+++ src/extract_features_baseline.py
@@ -4,6 +4,7 @@ import cv2
 from feature_asymmetry import get_assymetry
 from feature_border import get_border
 from feature_texture import mean_gradient
+from feature_diameter import get_diameter
 
 
 def extract_all():
@@ -14,18 +15,22 @@ def extract_all():
     results = []
 
     for index, row in metadata.iterrows():
-        img_id = row["img_id"].str[:-4] 
+        img_id = os.path.splitext(row["img_id"])[0] 
 
         img = cv2.imread(f"../data/imgs/{img_id}.png")
-        mask = cv2.imread(f"../data/masks/{img_id}_mask.png")
+        mask = cv2.imread(f"../data/masks/{img_id}_mask.png", cv2.IMREAD_GRAYSCALE)
 
         feature_a = get_assymetry(mask)
         feature_b = get_border(mask)
+        feature_t = mean_gradient(img, mask)
+        feature_d = get_diameter(mask)
 
 
         results.append({"img_id": img_id,
             "asymmetry": feature_a,
             "border": feature_b,
+             "texture": feature_t,
+            "diameter": feature_d,
             "cancer": row["cancer"]})
     
     df_features = pd.DataFrame(results)
